ef17a1d0904c95089eb1497af9a6b03f47109a0b
mrpinkzh <mrpinkzh@gmail.com>
2015-06-27 10:53:10 +0200

    Integrated generic property assertion.

---
+++ ExAs/GenericPropertyAssertionExtensions.cs
@@ -0,0 +1,14 @@
+﻿using ExAs.Assertions.Generic;
+using ExAs.Assertions.GenericValueAssertions;
+
+namespace ExAs
+{
+    public static class GenericPropertyAssertionExtensions
+    {
+        public static ObjectAssertion<T> IsNull<T, TProperty>(
+            this GenericPropertyIdentifier<T, TProperty> propertyIdentifier)
+        {
+            return propertyIdentifier.SetAssertion(new IsNullAssertion<TProperty>());
+        } 
+    }
+}
---
+++ ExAs/PropertyIdentificationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using ExAs.Assertions.Generic;
+
+namespace ExAs
+{
+    public static class PropertyIdentificationExtensions
+    {
+        public static GenericPropertyIdentifier<T, TProperty> Property<T, TProperty>(
+            this ObjectAssertion<T> item,
+            Expression<Func<T, TProperty>> propertyExpression)
+        {
+            var identifier = new GenericPropertyIdentifier<T, TProperty>(propertyExpression, item);
+            item.AddPropertAssertion(identifier);
+            return identifier;
+        }
+    }
+}
