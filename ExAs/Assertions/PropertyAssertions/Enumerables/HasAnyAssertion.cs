3dc6e8d1aede8796ee0e5b3b5687d0bd183edb93
Manuel Markwalder <manuel.markwalder@contractors.roche.com>
2015-09-14 15:29:50 +0200

    Extended HasAnyAssertion.

---
+++ ExAs/Assertions/PropertyAssertions/Enumerables/HasAnyAssertion.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using ExAs.Results;
+using ExAs.Utils;
+using ExAs.Utils.SystemExtensions;
+
+namespace ExAs.Assertions.PropertyAssertions.Enumerables
+{
+    public class HasAnyAssertion<TElement> : IAssertValue<IEnumerable<TElement>>
+    {
+        private readonly ObjectAssertion<TElement> childAssertion;
+
+        public HasAnyAssertion(ObjectAssertion<TElement> childAssertion)
+        {
+            this.childAssertion = childAssertion;
+        }
+
+        public ValueAssertionResult AssertValue(IEnumerable<TElement> actual)
+        {
+            string expectation = ComposeLog.Expected("at least 1 match");
+            if (actual == null)
+                return new ValueAssertionResult(
+                    false,
+                    actual.ToValueString(), 
+                    expectation);
+
+            var actualList = actual.ToList();
+            if (!actualList.Any())
+                return new ValueAssertionResult(
+                    false,
+                    actualList.ToValueString(),
+                    expectation);
+
+            IReadOnlyCollection<ObjectAssertionResult> results = actualList.Map(item => childAssertion.Assert(item));
+            int amountOfSucceededResults = results.Count(r => r.succeeded);
+            return new ValueAssertionResult(
+                amountOfSucceededResults > 0, 
+                string.Join(Environment.NewLine,
+                            new[] { Matches(amountOfSucceededResults) }
+                                .Concat(results.Select(r => r.log))), 
+                string.Join(Environment.NewLine,
+                            new[] { expectation }
+                                .Concat(results.Select(r => r.expectation))));
+        }
+
+        private static string Matches(int amount)
+        {
+            if (amount == 1)
+                return "<1 match>";
+            return string.Format("<{0} matches>", amount);
+        }
+    }
+}
