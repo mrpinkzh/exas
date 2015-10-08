61fb79cbbb3fb24880edfb12815a46eae085d18f
mrpinkzh <mrpinkzh@gmail.com>
2015-10-08 10:02:27 +0200

    implemented integer equal-to feature tests

---
+++ src/ExAs.Tests/Api/IntegerAssertionFeature.cs
@@ -0,0 +1,30 @@
+﻿using ExAs.Results;
+using ExAs.Utils;
+using NUnit.Framework;
+
+namespace ExAs.Api
+{
+    [TestFixture]
+    public class IntegerAssertionFeature
+    {
+        private readonly Ninja naruto = new Ninja();
+
+        [Test]
+        public void IsEqualTo_Expect12_Get12_ShouldPass()
+        {
+            ObjectAssertionResult result = naruto.Evaluate(n => n.Property(x => x.Age).IsEqualTo(12));
+            result.ExAssert(r => r.p(x => x.succeeded)  .IsTrue()
+                                  .p(x => x.log)        .IsEqualTo("Ninja: ( )Age = 12")
+                                  .p(x => x.expectation).IsEqualTo("(expected: 12)"));
+        }
+
+        [Test]
+        public void IsEqualTo_Expect13_Get12_ShouldFail()
+        {
+            ObjectAssertionResult result = naruto.Evaluate(n => n.p(x => x.Age).IsEqualTo(13));
+            result.ExAssert(r => r.p(x => x.succeeded)  .IsFalse()
+                                  .p(x => x.log)        .IsEqualTo("Ninja: (X)Age = 12")
+                                  .p(x => x.expectation).IsEqualTo("(expected: 13)"));
+        }
+    }
+}
---
+++ src/ExAs.Tests/Utils/ExasShortcutNotationExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq.Expressions;
+using ExAs.Assertions;
+
+namespace ExAs.Utils
+{
+    public static class ExasShortcutNotationExtensions
+    {
+        public static PropertyAssertion<T, TProperty> p<T, TProperty>(
+            this ObjectAssertion<T> objectAssertion, 
+            Expression<Func<T, TProperty>> expession)
+        {
+            var propertyAssertion = new PropertyAssertion<T, TProperty>(expession, objectAssertion);
+            objectAssertion.AddPropertyAssertion(propertyAssertion);
+            return propertyAssertion;
+        }
+    }
+}
