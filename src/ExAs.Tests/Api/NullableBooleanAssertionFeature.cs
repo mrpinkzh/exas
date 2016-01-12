9a7656ad3a966627e085563dabdd3d41f9e4d3d7
Manuel Markwalder <manuel.markwalder@contractors.roche.com>
2016-01-12 16:13:16 +0100

    implemented nullable-bool is-null assertion #9

---
+++ src/ExAs.Tests/Api/NullableBooleanAssertionFeature.cs
@@ -0,0 +1,45 @@
+﻿using ExAs.Utils;
+using NUnit.Framework;
+
+namespace ExAs.Api
+{
+    [TestFixture]
+    public class NullableBooleanAssertionFeature
+    {
+        [Test]
+        public void IsNull_OnVirginNinja_ShouldPass()
+        {
+            // arrange
+            var virginNinja = new FightNinja(null);
+
+            // act
+            var result = virginNinja.Evaluate(n => n.Property(x => x.defeated).IsNull());
+
+            // assert
+            result.ExAssert(r => r.Fullfills(true, "FightNinja: ( )defeated = null", "(expected: null)"));
+        }
+
+        [Test]
+        public void IsNull_OnDefeatedNinja_ShouldFail()
+        {
+            // act
+            var defeatedNinja = new FightNinja(true);
+
+            // act
+            var result = defeatedNinja.Evaluate(n => n.Property(x => x.defeated).IsNull());
+
+            // assert
+            result.ExAssert(r => r.Fullfills(false, "FightNinja: (X)defeated = True", "(expected: null)"));
+        }
+
+        private class FightNinja : Ninja
+        {
+            public readonly bool? defeated;
+
+            public FightNinja(bool? defeated)
+            {
+                this.defeated = defeated;
+            }
+        }
+    }
+}
---
+++ src/ExAs/NullableBooleanPropertyAssertionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using ExAs.Assertions;
+using ExAs.Assertions.ObjectAssertions;
+
+namespace ExAs
+{
+    public static class NullableBooleanPropertyAssertionExtensions
+    {
+        public static ObjectAssertion<T> IsNull<T>(this PropertyAssertion<T, bool?> property)
+        {
+            return property.SetAssertion(new IsNullAssertion<bool?>());
+        }
+    }
+}
