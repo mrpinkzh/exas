66e8334c1072a7e9f7ed3f8dea50b114a9fc47a1
Manuel Markwalder <manuel.markwalder@aias.ch>
2016-01-20 11:16:02 +0100

    introduced checks for the comparable assertions. #8

--- src/ExAs/Utils/TimeSpanFunctions.cs
+++ src/ExAs/Utils/TimeSpanFunctions.cs
@@ -8,5 +8,15 @@ namespace ExAs.Utils
         {
             return new TimeSpan(0, 0, amount);
         }
+
+        public static TimeSpan Minutes(this int amount)
+        {
+            return new TimeSpan(0, amount, 0);
+        }
+
+        public static TimeSpan Hours(this int amount)
+        {
+            return new TimeSpan(amount, 0, 0);
+        }
     }
 }
