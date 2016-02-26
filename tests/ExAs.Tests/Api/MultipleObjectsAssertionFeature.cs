dcf48c501a6033886d11b60f08c496767323f3dd
Manuel Markwalder <manuel.markwalder@aias.ch>
2016-02-26 08:47:51 +0100

    changed multiple-objects-assertion-feature to be culture invarriant.

--- tests/ExAs.Tests/Api/MultipleObjectsAssertionFeature.cs
+++ tests/ExAs.Tests/Api/MultipleObjectsAssertionFeature.cs
@@ -5,6 +5,9 @@ using NUnit.Framework;
 
 namespace ExAs.Api
 {
+    using System.Globalization;
+    using System.Threading;
+
     [TestFixture]
     public class MultipleObjectsAssertionFeature
     {
@@ -13,11 +16,16 @@ namespace ExAs.Api
         [Test]
         public void WithNarutosDojo_AndValidAssertions_ShouldPass()
         {
+            // arrange
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("de");
+
+            // act
             ObjectAssertionResult result = narutosDojo.Evaluate(
                 d => d.Member(x => x.Master).Fulfills(n => n.Member(x => x.Name).IsEqualTo("Naruto")
                                                                  .Member(x => x.Age) .IsEqualTo(12))
                       .Member(x => x.Founded).IsEqualTo(new DateTime(1515, 11, 1)));
 
+            // assert
             Assert.IsTrue(result.succeeded);
             Console.Out.WriteLine(result.PrintLog());
             Assert.AreEqual("Dojo: ( )Master  = Ninja: ( )Name = 'Naruto' (expected: 'Naruto')".NewLine()
@@ -29,17 +37,21 @@ namespace ExAs.Api
         [Test]
         public void ExpectingTakahashisDojo_OnNarutosDojo_ShouldFail()
         {
+            // arrange
+            Thread.CurrentThread.CurrentCulture = CultureInfo.GetCultureInfo("en");
+
             // act
             var result = narutosDojo.Evaluate(
                 d => d.Member(x => x.Master).Fulfills(n => n.Member(x => x.Name).IsNotNull()
                                                               .Member(x => x.Age) .IsEqualTo(26))
                       .Member(x => x.Founded).IsEqualTo(new DateTime(1515, 11, 1)));
 
+            // assert
             Assert.IsFalse(result.succeeded);
             Console.Out.WriteLine(result.PrintLog());
             Assert.AreEqual("Dojo: (X)Master  = Ninja: ( )Name = 'Naruto' (expected: not null)".NewLine()
                        .Add("                          (X)Age  = 12       (expected: 26)").NewLine()
-                       .Add("      ( )Founded = 01.11.1515 00:00:00       (expected: 01.11.1515 00:00:00)"),
+                       .Add("      ( )Founded = 11/1/1515 12:00:00 AM     (expected: 11/1/1515 12:00:00 AM)"),
                        result.PrintLog());
         }
     }
