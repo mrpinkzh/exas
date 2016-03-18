b332c6a1aaa8735e1502944405945e45d5a39b28
Manuel Markwalder <mrpinkzh@gmail.com>
2016-03-18 06:12:54 +0100

    added exas short syntax.

---
+++ src/ExAs/ExasShort.cs
@@ -0,0 +1,36 @@
+﻿using ExAs.Assertions;
+using ExAs.Results;
+
+namespace ExAs
+{
+    public static class ExasShort
+    {
+        public static void ExAssert<T>(T instance, IAssert<T> assertion)
+        {
+            ObjectAssertionResult result = Evaluate(instance, assertion);
+            if (result.succeeded)
+                return;
+            throw new ExtendedAssertionException(result);
+        }
+
+        public static ObjectAssertionResult Evaluate<T>(T instance, IAssert<T> assertion)
+        {
+            return assertion.Assert(instance);
+        }
+
+        public static IAssert<T> IsNotNull<T>()
+        {
+            return new ObjectAssertion<T>().IsNotNull();
+        }
+
+        public static IAssert<T> OfType<T>()
+        {
+            return new ObjectAssertion<T>();
+        } 
+
+        public static IAssert<T> Has<T>()
+        {
+            return new ObjectAssertion<T>();
+        } 
+    }
+}
---
+++ tests/ExAs.Tests/Api/ShortSyntax/ShortSyntaxFeature.cs
@@ -0,0 +1,42 @@
+﻿using ExAs.Results;
+using ExAs.Utils;
+using NUnit.Framework;
+using static ExAs.ExasShort;
+using static ExAs.Utils.Creation.CreateNinjas;
+
+namespace ExAs.Api.ShortSyntax
+{
+    public class ShortSyntaxFeature
+    {
+        [Test]
+        public void ExpectingNotNull_OnNaruto_ShouldPass()
+        {
+            // act
+            var result = Evaluate(Naruto(), IsNotNull<Ninja>());
+
+            // assert
+            result.Evaluate(r => r.Member(x => x.succeeded).IsTrue()
+                                  .Member(x => x.PrintLog()).IsEqualTo("Ninja: Name = 'Naruto' (expected: not null)"));
+        }
+
+        [Test]
+        public void ExpectingOfTypeNinjaNaruto_OnNaruto_ShouldPass()
+        {
+            // act
+            var result = Evaluate(Naruto(), OfType<Ninja>()
+                                                .Member(x => x.Name).IsEqualTo("Naruto"));
+        }
+
+        [Test]
+        public void ExpectingNaruto_OnKakshi_ShouldFail()
+        {
+            // act
+            var result = Evaluate(Kakashi(), Has<Ninja>().Member(x => x.Name).IsEqualTo("Naruto")
+                                                         .Member(x => x.Age) .IsEqualTo(12));
+
+            // assert
+            ExAssert(result, Has<ObjectAssertionResult>().Member(x => x.succeeded).IsFalse()
+                                                         .Member(x => x.log)      .Printout());
+        }
+    }
+}
