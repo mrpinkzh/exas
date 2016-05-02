3bea17cb7e3ab88245bd8c4126dfa1e7eb773d3e
Manuel Markwalder <manuel.markwalder@aias.ch>
2016-05-02 11:04:24 +0200

    implemented the first two tests for the enumerable-has-none assertion. #53

---
+++ tests/ExAs.Tests/Api/Enumerables/Enumerable_HasNone_WithPredicate_Feature.cs
@@ -0,0 +1,39 @@
+﻿using ExAs.Results;
+using NUnit.Framework;
+using static ExAs.Utils.ComposeLog;
+using static ExAs.Utils.Creation.CreateNinjas;
+using static System.Environment;
+
+namespace ExAs.Api.Enumerables
+{
+    [TestFixture]
+    public class Enumerable_HasNone_WithPredicate_Feature
+    {
+        [Test]
+        public void ExpectingNoneStartingWithSw_OnWeaponlessNinja_ShouldSucceed()
+        {
+            // act
+            Result result = WeaponlessNinja().Evaluate(n => n.Member(x => x.Weapons).HasNone(w => w.StartsWith("Sw")));
+
+            // assert
+            result.ExAssert(r => r.Member(x => x.succeeded).IsTrue()
+                                  .Member(x => x.actual)    .IsEqualTo("WeaponedNinja: ( )Weapons = <empty>")
+                                  .Member(x => x.expectation).IsEqualTo(Expected("0 matches")));
+        }
+
+        [Test]
+        public void ExpectingNoneStartingWithShu_OnNinjaWithShuriken_ShouldFail()
+        {
+            // act
+            Result result = NinjaWithTwoWeapons().Evaluate(n => n.Member(x => x.Weapons).HasNone(w => w.StartsWith("Shu")));
+
+            // assert
+            result.ExAssert(r => r.Member(x => x.succeeded)  .IsFalse()
+                                  .Member(x => x.actual)     .IsEqualTo($"WeaponedNinja: (X)Weapons = <1 match>")//{NewLine}" +"")
+                                                                        //$"                            'Sword'{NewLine}" +
+                                                                        //$"                            'Shuriken'")
+                                  .Member(x => x.expectation).IsEqualTo($"1 match{NewLine}" +
+                                                                        $""));
+        }
+    }
+}
